9b5b7491c25f0b7d7665ddf1195f4472c65d1ca4
Kerry Ritter <kritter@lumeris.com>
2020-08-28 12:42:57 -0500

    refactor(MultipleAppSupport) - updating exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from './nest-azure-ad-jwt-validator.module';
 export * from './azure-token-validation';
 export * from './models';
+export * from './module-config';
 export * from './guards';
 export * from './decorators';
+
